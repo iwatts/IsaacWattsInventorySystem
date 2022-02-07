29cbd5a206c9c12b32c7f14c99b20eff0dd0b477
Isaac <isaac.r.watts@gmail.com>
2022-02-06 17:29:25 -0700

    Add project files.

---
+++ IsaacWattsInventorySystem/ClassApp-Parts.sql
@@ -0,0 +1,111 @@
+CREATE TABLE mytable(
+   id          INTEGER  NOT NULL PRIMARY KEY 
+  ,name        VARCHAR(21) NOT NULL
+  ,inStock     INTEGER  NOT NULL
+  ,price       VARCHAR(6) NOT NULL
+  ,min         INTEGER  NOT NULL
+  ,max         INTEGER  NOT NULL
+  ,in_out      VARCHAR(5) NOT NULL
+  ,machineID   VARCHAR(36) NOT NULL
+  ,companyName VARCHAR(13) NOT NULL
+);
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (1,'sagittis nam',40,'$3.13',4,14,'true','b7186f4e-ab86-4472-9c7c-a1cfa34b5ab1','Brainbox');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (2,'erat',33,'$1.21',2,19,'true','fd12b9eb-c12c-4ff5-914a-e60bed0a5aac','Jaxbean');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (3,'congue eget',383,'$12.41',5,20,'true','cdac000e-a08e-4f83-83da-5e0e73f8f875','Babblestorm');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (4,'volutpat',270,'$16.67',2,7,'true','e923a126-7803-4365-91de-82f69eb6e7fb','Skilith');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (5,'felis',23,'$7.83',5,14,'true','9a7d9da8-73e0-4f09-abae-f52c3b570f55','Photobean');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (6,'lorem',136,'$4.52',1,18,'true','a14043fd-ba96-4891-bc9e-0c738a5f1ed3','Quinu');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (7,'vulputate nonummy',348,'$15.09',5,7,'true','0cb55f47-f369-4cf4-84c8-511a8c04b819','Babblestorm');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (8,'maecenas leo',133,'$15.64',2,19,'false','34636802-2c19-466e-86e4-120b1ebdd32f','Devify');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (9,'quis justo',429,'$17.56',3,12,'true','9bc4c0e0-34cb-4a29-94b6-35694753baed','Browsebug');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (10,'orci',463,'$12.92',1,17,'true','67c08b6f-6e1b-4fe3-a059-4a0e3d4592bc','Rhyloo');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (11,'etiam pretium',451,'$1.26',3,6,'false','3a31360e-8086-4bff-9e37-bc99effdd2c0','Youopia');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (12,'ultrices posuere',335,'$6.24',4,15,'false','2b426f11-b57f-45b2-bc93-9d48a9403984','Vitz');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (13,'rhoncus',285,'$13.70',5,6,'true','6870659e-770e-41ec-bb26-b756278725d7','Meejo');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (14,'consequat',462,'$10.88',1,14,'true','a44691c7-03bd-419e-a306-e4192a80a51a','Tazzy');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (15,'erat vestibulum',500,'$5.30',2,17,'false','73c6e09f-a11d-4e3f-b24a-22e4ef03152b','Skyndu');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (16,'volutpat quam',139,'$16.51',1,8,'false','21c2dfdf-f739-4977-8390-e481912c27d1','Demizz');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (17,'donec vitae',13,'$9.59',4,10,'false','b7e3effe-d720-46a3-adb4-748d03952958','Miboo');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (18,'consequat',248,'$17.38',3,8,'true','ffa438b5-6d9e-4019-bcd0-ae455bd68a7b','Dynabox');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (19,'donec',210,'$9.10',3,13,'false','5d467d2c-2cfe-4d56-ad7a-546af7920bf8','Demizz');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (20,'facilisi cras',264,'$7.93',5,11,'true','6d72e20c-dd6f-4103-8436-59d9c4e40847','Shufflebeat');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (21,'nibh in',492,'$10.44',5,9,'false','59548fe5-fb4a-4d83-b477-769a2285a214','Buzzdog');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (22,'nascetur',317,'$11.88',3,9,'false','a5182e1c-b0f5-4ebd-a218-80d38beebe55','Katz');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (23,'donec diam',399,'$13.98',3,16,'true','ce7337d5-5cd0-4373-9060-8c2a0a9b806b','Avavee');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (24,'quis',167,'$9.34',1,16,'false','b69c383e-52c7-40bd-a9e2-a1c15fa507e3','Bluejam');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (25,'mattis nibh',12,'$17.99',4,14,'true','a7ef5f4d-6478-4b15-b02f-ec22c01a6e05','Thoughtworks');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (26,'nullam',14,'$11.23',4,19,'true','ec500d71-be07-4a63-9811-dda53ea05d08','Avavee');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (27,'quam',135,'$2.67',1,15,'true','d903bbd7-46bd-44f1-918f-9745338e1776','Youbridge');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (28,'justo',16,'$3.54',4,19,'false','7c80a541-4c58-4b4b-8b11-6d2ae448a8b5','Blogtag');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (29,'libero non',167,'$13.02',4,17,'true','e57bb555-d11c-4b54-ad8f-1ab5b58b885f','Reallinks');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (30,'nulla',323,'$18.17',4,16,'false','d64a593d-b2be-4fd3-8137-617733c59007','Flashset');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (31,'nulla',469,'$17.14',3,12,'true','c0a78083-a3e0-44ff-bc2b-4adb660902a1','Buzzshare');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (32,'lobortis sapien',442,'$11.97',1,8,'false','ddade597-5030-4cde-87c5-fe249b96140d','Katz');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (33,'sit',219,'$8.97',5,20,'true','3c2eeef4-7c92-483b-9a47-d64a1fdce4dd','Yabox');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (34,'erat curabitur',283,'$11.01',3,15,'true','621f2aa0-1698-4861-a1b5-8ddd91300d5b','Gabtune');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (35,'quam',242,'$19.97',5,18,'false','9e55c1b0-e6b4-4eac-ad6f-f94e89558a02','Plambee');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (36,'sit',204,'$17.55',4,16,'false','64431758-063f-43fe-b8ee-16425a8bd1bb','Realbuzz');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (37,'ante',214,'$8.94',3,14,'true','8da82a4f-ca84-4d5d-9ab7-a7727b5ec721','Kazio');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (38,'lorem ipsum',307,'$13.08',5,9,'false','1456a808-8ead-40dc-ad4f-a184ddce813b','Viva');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (39,'tempus',353,'$14.49',4,8,'true','6981bff3-5462-4992-a855-ff933e36bbc8','Topicshots');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (40,'mattis',61,'$6.26',5,10,'true','4113da12-3aa1-423d-bcf0-d020c0f0d3f9','Yacero');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (41,'ultrices aliquet',464,'$9.27',3,9,'true','62e643cb-530e-45ba-b2b5-4adafbdfd0d3','Meembee');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (42,'condimentum curabitur',28,'$1.08',1,20,'true','58ffedf0-afc9-4006-abdd-4f77f6765b4f','Talane');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (43,'rutrum',195,'$1.68',5,15,'true','bb81b09b-e608-47c2-84a3-f0650008b963','Yodel');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (44,'lobortis',273,'$6.72',1,14,'true','9b243793-ca20-40f9-a6eb-114c3e0ee559','Yodoo');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (45,'sapien sapien',55,'$12.60',2,19,'true','2493e110-6cfe-44a5-b2be-1b40615e2630','Flashspan');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (46,'eget vulputate',274,'$8.43',3,12,'true','c2576c0c-75ea-4342-b505-d8679c9caecb','Realpoint');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (47,'massa',36,'$12.31',2,6,'false','538ff4de-a11c-4699-9576-6d779162f29a','Janyx');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (48,'fusce consequat',265,'$14.20',3,20,'true','b4d4ceca-3171-4bb8-b271-f6a4e20c310d','Yambee');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (49,'id turpis',156,'$7.82',2,12,'false','9917fd94-9385-490d-9228-f58e4d8b9006','Realmix');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (50,'turpis',181,'$1.90',1,13,'true','1d34993b-7ed9-41bd-9bb6-3f986b204218','Fliptune');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (51,'platea dictumst',194,'$10.46',1,14,'true','6d550fd0-8360-4f2a-9553-d756cdd7f535','Thoughtbridge');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (52,'eget',340,'$1.18',5,13,'false','a9b16400-a5c1-4f58-a0ce-c9fe65d5812f','Youtags');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (53,'nunc',164,'$9.39',5,10,'false','b8089e23-37d9-4377-82e9-81445bdaaee4','Vitz');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (54,'consequat',473,'$1.41',1,11,'false','eef58ef3-4902-4d03-a992-a8275d4aee6e','Jazzy');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (55,'dolor sit',455,'$5.33',1,13,'false','6fa3e127-d8a5-41b4-8176-3ae69b85109c','Jabbercube');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (56,'luctus',97,'$16.39',3,10,'true','7ac5ccbc-24ca-4909-9171-c73bd6adb296','Twitterbridge');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (57,'diam',467,'$18.73',1,14,'false','ab1fc4ee-6dc1-4fa4-9fdb-f66cf273b2f8','Centidel');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (58,'platea dictumst',38,'$18.83',3,10,'true','3aad853c-494e-4638-a48e-41a24281d383','Rooxo');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (59,'at dolor',113,'$9.92',5,16,'true','93895d9b-49d6-412e-8773-ede266db5402','Kimia');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (60,'mus',351,'$9.36',5,19,'false','092d22da-5e50-4bc2-809c-2159e06e0400','Ailane');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (61,'scelerisque',374,'$10.00',5,10,'true','f239b9d0-dae6-404a-911f-86c0e91ee041','Tagcat');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (62,'sed vestibulum',315,'$4.90',1,6,'false','facc26ce-cda5-44f2-8ee5-6f0c243b0dc7','Gigaclub');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (63,'aliquam lacus',28,'$15.85',1,20,'true','154d29de-e1d8-420f-8f29-6c2bd7c0618f','Kimia');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (64,'hac',269,'$18.51',5,11,'true','d2a57708-c548-44da-9062-a374d97710bd','Avavee');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (65,'libero',97,'$17.98',4,11,'true','9165a6ce-ee1d-476e-bd2d-b46cc7922d29','Devpoint');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (66,'facilisi cras',64,'$11.61',2,8,'true','92ca8e64-8a3d-4126-a4c6-fe4a46759c5a','Jabbersphere');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (67,'posuere cubilia',53,'$18.92',2,7,'true','0b9ec4b2-bd48-4441-995a-dea6c51467c4','Pixoboo');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (68,'non',173,'$15.21',1,8,'true','d12ce4f9-5047-480d-8b12-5d3b11aff15b','Eadel');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (69,'velit',42,'$14.16',1,7,'true','f3aebde0-db78-43ed-9f40-63e386585267','Gabtype');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (70,'sodales',483,'$8.41',1,9,'true','8139ebc8-9e7d-4a81-80c2-c25e44c1284c','Eamia');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (71,'in quis',201,'$19.29',4,20,'false','5422da3d-1aae-41f3-bf56-c0f3b27ab43c','Leenti');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (72,'cum sociis',167,'$18.76',2,12,'false','c547bcba-086e-4582-8c48-d566cd50cbe9','Snaptags');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (73,'mauris',292,'$5.19',3,18,'true','9486951c-1152-482d-a397-5db0ad25c5ac','Twitterworks');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (74,'vestibulum',429,'$2.96',5,9,'false','da03fa4a-57d9-49bb-ac7e-e98892a1a9b9','Trupe');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (75,'pede',181,'$13.86',1,7,'true','338059e2-398d-4fda-957f-00f925e2d91a','Oyope');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (76,'sit',275,'$12.57',4,18,'false','a2251d43-d9b7-4e91-993a-5ce622e66aa0','Youbridge');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (77,'cras pellentesque',308,'$6.65',5,10,'false','b36c8320-d23b-4889-b4e9-be0b1b7505d2','Blogspan');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (78,'pretium iaculis',143,'$3.15',2,15,'false','dd345814-4fcf-436d-a22e-20e84b73724e','Buzzdog');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (79,'ornare imperdiet',296,'$5.02',2,13,'true','1e26af98-2946-43e2-8248-27cbb19534dd','Devify');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (80,'pretium',186,'$8.16',5,15,'false','edb56faf-7da1-4222-9a69-e7bf7eedd124','Feedspan');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (81,'rhoncus',405,'$6.48',4,16,'true','54efd864-c2bf-46ee-a794-bb07f3047754','Yakijo');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (82,'lectus',53,'$10.62',2,18,'true','200dce77-925f-4a3c-a6a6-80d17d3a80ef','Browseblab');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (83,'id',90,'$19.64',3,15,'true','6680a038-bb8a-4fa9-8747-707fbad6095e','Jabbersphere');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (84,'tempus semper',13,'$10.25',4,11,'false','26e32eef-2009-42aa-9b79-ca1bd8bf07d0','Topiczoom');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (85,'hac habitasse',396,'$14.76',1,16,'true','5e848259-b058-4aea-8074-e45831b3ca17','Roomm');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (86,'vel accumsan',79,'$4.63',5,14,'true','651db152-0c88-40bb-93fa-b59af9a26899','Linklinks');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (87,'duis',316,'$10.23',4,7,'false','fcd46a60-9702-42d0-8b96-f23d8da739c6','Blogtags');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (88,'et',140,'$4.64',5,13,'false','230b6bbb-dc1d-4a5c-945c-5ba2e287cee3','Fivespan');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (89,'molestie sed',224,'$9.01',1,10,'false','20ccdb7b-0a57-4d19-8d67-8ddf518a3b95','Myworks');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (90,'commodo',413,'$15.68',5,10,'false','4987d094-ddca-4805-98e9-a9ade44f2a90','Topiczoom');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (91,'diam',448,'$17.07',4,8,'true','752123b1-7600-4cb5-83e2-e97373b1c27a','Twitterbeat');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (92,'orci',277,'$19.99',2,19,'false','bd9046fc-aab4-4056-9e7f-25cd4c526d78','Kimia');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (93,'massa',286,'$13.27',3,6,'false','eb24af4c-7f65-461c-8847-8ed5589f0259','Jabbersphere');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (94,'faucibus',402,'$9.26',3,10,'true','41e8713c-3bf7-4ded-aaef-537bee430744','Einti');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (95,'phasellus id',183,'$12.56',4,18,'false','db617ba1-ab83-4956-a3bb-5effac4ef18a','Yata');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (96,'sociis natoque',75,'$18.46',1,10,'false','825f9fb5-1657-4fc6-a09a-d521b60b614a','Meezzy');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (97,'tellus',391,'$17.72',1,18,'true','fca90c10-ecdf-4768-a1a7-2d33c1137a31','Dabshots');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (98,'venenatis turpis',85,'$14.50',1,12,'true','8bc8495f-38ad-486b-a005-e1c0f54f5634','Latz');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (99,'natoque penatibus',35,'$17.09',2,11,'true','f692287c-bdfe-4daf-882c-f2248172e687','Dabvine');
+INSERT INTO mytable(id,name,inStock,price,min,max,in_out,machineID,companyName) VALUES (100,'sit amet',243,'$1.46',4,7,'false','cad72a5c-4bba-4fa0-98c3-9ef66b2e381d','Dabfeed');
