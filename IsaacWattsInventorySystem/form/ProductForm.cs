c457e5b85ce3c5ea556fd7e194a3cdb3910a70e8
Isaac <isaac.r.watts@gmail.com>
2022-04-03 17:40:24 -0600

    imports

--- IsaacWattsInventorySystem/form/ProductForm.cs
+++ IsaacWattsInventorySystem/form/ProductForm.cs
@@ -1,14 +1,6 @@
 ﻿using IsaacWattsInventorySystem.models;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Globalization;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace IsaacWattsInventorySystem.forms
 {
